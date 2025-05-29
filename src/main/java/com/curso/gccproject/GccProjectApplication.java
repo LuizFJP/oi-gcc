6b1f0cf234045ee917a64456991ddd0375ba0517
LuizFJP <luizfjportela@gmail.com>
2025-05-29 19:46:48 -0300

    comecando o projeto

---
+++ src/main/java/com/curso/gccproject/GccProjectApplication.java
@@ -0,0 +1,13 @@
+package com.curso.gccproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GccProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GccProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/curso/gccproject/GccProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.gccproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GccProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
